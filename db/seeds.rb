42753b86e0ee3f2d73e736deb93c3d76dbb288f1
Cande Garcia <cruztoyota@gmail.com>
2015-08-09 22:57:20 -0500

    Checkpoint 33 completed, crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,38 @@
  require 'faker'
  
- # Create Posts
+  #Create Posts
  50.times do
    Post.create!(
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
-   )
+     )  
  end
  posts = Post.all
- 
+
  # Create Comments
  100.times do
    Comment.create!(
      post: posts.sample,
      body: Faker::Lorem.paragraph
-   )
+   )  
  end
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ 
+ #Second post
+   Post.create!(
+    title: "Dog snacks",
+    body: "The best ingredients for the tastiest snacks."
+  )
+
+ #Second comment
+   Comment.create!(
+    post: post,
+    body: "This is a second post."
+   )
+  
+   puts "Unique post finished."
+   
+
