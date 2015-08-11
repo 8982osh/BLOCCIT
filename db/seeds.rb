0b49059983c600ab2fce83931683f44375d2ffbf
Cande Garcia <cruztoyota@gmail.com>
2015-08-10 23:04:05 -0500

    Assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
  require 'faker'
- 
+#if Rails.env.development? 
+ Post.destroy_all
+ Comment.destroy_all
+#end
  # Create Posts
  50.times do
    Post.create!(
@@ -20,3 +23,29 @@
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ 
+ #Second post
+ #posts = Post.where(title: 'Dog snacks').where(body: 'The best ingredients for the tastiest snacks')
+ #if posts.count > 0
+    #code
+ #end
+ post = Post.find_by(title: 'Dog snacks', body: 'The best ingredients for the tastiest snacks')
+ if post.nil?
+    Post.create!(
+     title: "Dog snacks",
+     body: "The best ingredients for the tastiest snacks"
+ )
+ else
+    puts "Post already exits"
+ end
+ 
+ comment = Comment.find_by(post_id: 1, body: 'Create posting for dog snacks')
+ if comment.nil? 
+    Comment.create!(
+      post_id: 1,
+      body: "Create posting for dog snacks"
+ )
+  else
+    puts "Comment already exists"
+ end
+ 
