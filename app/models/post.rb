d687f51d890228155bb5d362d4b1223f5944606e
Cande Garcia <cruztoyota@gmail.com>
2015-08-29 00:31:38 -0500

    Assignment 38 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ActiveRecord::Base
   belongs_to :user
   
   default_scope { order('created_at DESC')}
+  #end
+
+  
+  scope :ordered_by_title, -> { where(title: true) }
+  scope :ordered_by_reverse_created_at, -> { where(created_at: true) }
+  #scope :ordered_by_reverse_created_at, -> { where (created_at.first) }
+  #scope :ordered_by_reverse_created_at, -> { order("reverse_created_at") }
+  #end
+
+  #scope :ordered_by_title, -> { where(title: true) }
+  #scope :ordered_by_reverse_created_at, -> { order("reverse_created_at") }
 end
+  
