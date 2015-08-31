3ea06166f074fa8a087af64b739f2f31579d9a4f
Cande Garcia <cruztoyota@gmail.com>
2015-08-31 07:54:01 -0500

    In progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    #@posts = @user.posts
   end
 
   def new
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,29 @@
 class PostPolicy < ApplicationPolicy
+  
+  class Scope
+    #attr_reader :user, :post
+    
+    #def initialize(user, post)
+    #  @user = user
+    #  @post = post
+    #end
+    
+    #display diff sets of posts based on role of user. admin & mods see all posts,
+    #members only see their posts, guest see no posts
+    if @user.admin? || @user.moderator?
+      #@user == 'admin' || @user == 'moderator'
+       @posts = Post.all
+       authorize @posts
+    if @user.member?
+      #@user == 'member'
+       # @user.posts = Post.find(params[:id])
+        authorize @post
+    else
+        @user == 'guest'
+        
+    end
+    end  
+  #end
 
   def index?
     true
