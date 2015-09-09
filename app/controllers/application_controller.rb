95b9aec04450a273b53ba33ab6fcabf0052e75ec
Cande Garcia <cruztoyota@gmail.com>
2015-09-08 19:07:09 -0500

    Will update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   rescue_from Pundit::NotAuthorizedError do |exception|
-    redirect_to root_url, alert: exception.message
+    #raise exception.message.inspect
+    redirect_to root_url, alert: 'you\'re not allowed there'
   end
   
   protected
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,31 +1,32 @@
 class PostPolicy < ApplicationPolicy
   
-  class Scope
-    attr_reader :user, :scope
-    
-  def initialize(user, scope)
-      @user = user
-      @scope = scope
-  end
-    
-    #display diff sets of posts based on role of user. admin & mods see all posts,
-    #members only see their posts, guest see no posts
-  def resolve
-    if @user.admin? || @user.moderator?
-       #@user == 'admin' || @user == 'moderator'
-       scope.all
-    else @user.member?
-       #@user == 'member'
-       scope.where(user: user)
-    #else
-    #   flash[:notice] = "Guests do not have access to view posts."
-    #end
-    end
-  end
- 
-
   def index?
     true
   end
+
+  class Scope
+    attr_reader :user, :scope
+    
+    def initialize(user, scope)
+        @user = user
+        @scope = scope
+    end
+      
+      #display diff sets of posts based on role of user. admin & mods see all posts,
+      #members only see their posts, guest see no posts
+    def resolve
+      if @user.admin? || @user.moderator?
+         #@user == 'admin' || @user == 'moderator'
+         scope.all
+      else
+         #@user == 'member'
+         scope.where(user: user)
+      #else
+      #   flash[:notice] = "Guests do not have access to view posts."
+      #end
+      end
+    end
+ 
+
   end
 end
