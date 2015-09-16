89fafdf0b2c0bc0b563f9702b4024b826cab0cf5
Cande Garcia <cruztoyota@gmail.com>
2015-09-15 23:28:26 -0500

    Complete interlude assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,8 @@ class PostsController < ApplicationController
   
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    #@post = Post.new(params.require(:post).permit(:title, :body))
+    @post = Post.new(post_params)
     @post.user = current_user
     @post.topic = @topic
     authorize @post
@@ -44,12 +45,19 @@ class PostsController < ApplicationController
    def update
      @topic = Topic.find(params[:topic_id])
      @post = Post.find(params[:id])
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
+     if @post = Post.new(post_params)
+       #if @post.update_attributes(params.require(:post).permit(:title, :body))
        flash[:notice] = "Post was updated."
        redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
      end
+   end
+   
+   private
+   
+   def post_params
+      params.require(:post).permit(:title, :body)
    end
 end
