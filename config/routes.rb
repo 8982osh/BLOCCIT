5e9d669a4e251ebbffb56a6cb56058bbeec1bfb9
Cande Garcia <cruztoyota@gmail.com>
2015-11-17 00:43:28 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,20 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
   
+#  resources :topics do
+#    resources :posts, except: [:index] do #nest comments under posts, only create routes needed
+#      resources :comments, only: [:create, :destroy] 
+#    end
+#  end
+
   resources :topics do
-    resources :posts, except: [:index] do #nest comments under posts, only create routes needed
-      resources :comments, only: [:create, :destroy] 
-    end
+    resources :posts, except: [:index]
   end
+  
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+  
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
