a5d8e1fba312da3e29525a6417e3480734423e22
Cande Garcia <cruztoyota@gmail.com>
2015-11-18 14:38:57 -0600

    Complete assignment 47 destory

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,38 @@
 class CommentsController < ApplicationController
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    #@topic = Topic.find(params[:topic_id])
+    #@post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
     @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
+    @comment.post = @post
     
-    #authorize @comment
+    authorize @comment
     if @comment.save
-     redirect_to [@topic, @post], notice: "Comment was saved."
+     redirect_to [@post.topic, @post], notice: "Comment was saved."
     else
-     redirect_to [@topic, @post], notice:  "Error saving comment."
+     redirect_to [@post.topic, @post], notice:  "Error saving comment."
+     
+     #render 'post/show'
+        
     end
   end
   
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    #@topic = Topic.find(params[:topic_id])
+    #@post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @topic = @post.topic
     @comment = @post.comments.find(params[:id])
     
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
   
