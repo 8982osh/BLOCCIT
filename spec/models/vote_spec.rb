6478adae18a8f1601b1eb767f49dc41515c9b9ea
Cande Garcia <cruztoyota@gmail.com>
2015-11-21 00:13:44 -0600

    In progress

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,22 +1,15 @@
 describe Vote do
   describe "validations" do
+    
+    before do
+      2.times { @post.votes.create(value: 1) }
+      2.times { @post.votes.create(value: -1) }
+    end
+    
     describe "value validation" do
-      
-      describe '#' do
-        it "only allows -1 or 1 as values" do
-        e  nd
-        end
-      
-     describe '#yes_votes' do
-       it "counts the number of yes votes = 1" do
-       expect( @post.up_votes ).to eq(1)
-       end
-     end
-      
-     describe '#no_votes' do
-       it "count the number of no votes = -1" do
-       expect( @post.no_votes ).to eq(-1)
-     end
+      it "only allows -1 or 1 as values" do
+        expect( @post.votes ).to eq(-1) or eq(1)
+      end
     end
   end
 end
