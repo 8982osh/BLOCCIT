9033f38a5141004e72bc4a0bc26f5c81ac76c370
Cande Garcia <cruztoyota@gmail.com>
2015-11-23 00:16:01 -0600

    In progress

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,15 +1,5 @@
-describe Vote do
-  describe "validations" do
-    
-    before do
-      3.times { @post.votes.create(value: 1) }
-      2.times { @post.votes.create(value: -1) }
-    end
-    
-    describe "value validation" do
-      it "only allows -1 or 1 as values" do
-        expect( @post.votes ).to eq(-1) or eq(1)
-      end
-    end
-  end
+require 'rails_helper'
+
+RSpec.describe Vote, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
