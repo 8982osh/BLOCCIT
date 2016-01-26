6491e08eda0e66006064d3d25d979c7d284bb84e
Cande Garcia <cruztoyota@gmail.com>
2016-01-25 22:46:36 -0700

    User error with devise helper

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,18 +1,22 @@
-require 'rails_helpter'
+require 'rails_helper'
+require 'spec_helper'
 
 describe VotesController do 
 
   include TestFactories
 
+  include Devise::TestHelpers
+
   describe '#up_vote' do
     it 'adds an up-vote to the post' do
+      #request.env["HTTP_REFERER"] = '/'
 	  @user = authenticated_user
 	  @post = associated_post
 	  sign_in @user
 
 	  expect {
 	    post( :up_vote, post_id: @post.id )
-	  }.to change{ @post.up_votes }.by 1
+	    }.to change{ @post.up_votes }.by 1
 	end
   end	
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,4 +1,5 @@
  require 'rails_helper'
+ require 'spec_helper'
 
  describe Vote do
    describe "validations" do
@@ -18,8 +19,12 @@
   
   describe 'after_save' do
      it "calls `Post#update_rank` after save" do
-       post = associated_post
-       vote = Vote.new(value: 1, post: post)
+       #post = associated_post
+       #user = authenticated_user
+       #post = associated_post
+       #vote = Vote.new(value: 1, post: post)
+       sign_in @user
+
        expect(post).to receive(:update_rank)
        vote.save
     end
