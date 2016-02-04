fe68c836576399dec4aff6b57e32c5a4ea936b97
Cande Garcia <cruztoyota@gmail.com>
2016-02-03 23:00:47 -0700

    In progress

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,20 @@ describe User do
   
   include TestFactories
 
-  describe "#favorited(post)" do
-    xit "returns 'mil' if the user has not favorited the post" do
+  describe "#favorited(post)" 
+
+  before do
+    
+    @post = associated_post
+    @user = authenticated_user
+
+    it "returns 'nil' if the user has not favorited the post" do
+    	expect( @user.favorites.find_by_post_id(@post_id) ).to be_nil
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.create(@post_id)
+    	#expect( @user.favorites.find_by_post_id(@post_id) )
     end
   end
 end
