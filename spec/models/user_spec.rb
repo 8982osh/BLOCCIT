eda99465bb01f18b18217691d3560ab746832944
Cande Garcia <cruztoyota@gmail.com>
2016-02-04 22:19:32 -0700

    Unable to run

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,18 @@ describe User do
   include TestFactories
 
   describe "#favorited(post)" do
-    xit "returns 'mil' if the user has not favorited the post" do
+
+  	@post = associated_post 
+  	@user = authenticate_user
+  	#create(:user)
+
+    it "returns 'nil' if the user has not favorited the post" do
+    	expect( @user.favorites.find_by_post_id(@post_id) ).to be_nil
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+    	favorite = @user.favorites.where(post: @post).create
+    	expect( @user.favorites.find_by_post_id(@post_id) ).not_to be_nil
     end
   end
 end
