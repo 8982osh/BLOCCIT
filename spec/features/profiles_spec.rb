31314ba996caa59440b2d87bd8fc21eeef0aa26d
Cande Garcia <cruztoyota@gmail.com>
2016-02-15 20:00:04 -0700

    In progress

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe "Visiting profiles" do
 
-	include TestFactories
+  include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
 	before do
 	  @user = authenticated_user
@@ -22,6 +24,26 @@ describe "Visiting profiles" do
 	  	expect( page ).to have_content(@post.title)
 	  	expect( page ).to have_content(@comment.body)
 	  end
+    end
 
+describe "Test user visiting own profile" do
+
+	before do
+	  user = FactoryGirl.create(:user)
+	  login_as(user, scope: :user)
 	end
+	
+	describe "signed in" do
+	  it "shows profile" do
+	  	visit user_path(@user)
+	  	expect(current_path).to eq(user_path(@user))
+
+	  	expect( page ).to have_content(@user.name)
+	  	expect( page ).to have_content(@post.title)
+	  	expect( page ).to have_content(@comment.body)
+	  end
+    end
+end
+
+
 end
