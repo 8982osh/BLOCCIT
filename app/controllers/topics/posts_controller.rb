61229cb9cf2d4165c56e1cdee5947ce7eef615c3
Cande Garcia <cruztoyota@gmail.com>
2016-02-17 14:56:05 -0700

    Checkpoint 55 popular posts: completed

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,89 @@
+class Topics::PostsController < ApplicationController
+  
+  skip_before_action :flash_attack, only: [:index, :new]
+  
+  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    
+    #authorize @comment
+    #@comments = @post.comments.all
+    @comment = Comment.new(post_id: @post.id)
+    #raise @comments.inspect
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
+  end
+  
+  def create
+    @topic = Topic.find(params[:topic_id])
+    #@post = Post.new(params.require(:post).permit(:title, :body))
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.topic = @topic
+    authorize @post
+    @comment = Comment.new(params.require(:comment).permit(:title, :body))
+    @comment.user = user.present?
+    authorize @comment
+    
+    if @post.save
+      @post.create_vote
+      flash[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     authorize @post
+  end
+
+   def update
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     # reset the values for post
+     # save
+     if @post.update(post_params)
+       #if @post.update_attributes(params.require(:post).permit(:title, :body))
+       flash[:notice] = "Post was updated."
+       redirect_to [@topic, @post]
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+   end
+   
+   def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+ 
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
+   end
+   
+   private
+   
+   def post_params
+      #params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:name, :image)
+   end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,15 @@ class User < ActiveRecord::Base
   def voted(post)
     votes.where(post_id: post.id).first
   end
+
+  def self.top_rated
+    self.select('users.*') #Select all attributes of the user
+        .select('COUNT(DISTINCT comments.id) AS comments_count') #Count comments made by user
+        .select('COUNT(DISTINCT posts.id) AS posts_count') #Count posts made by user
+        .select('COUNT(DISTINCT comments.id) + COUNT(DISTINCT posts.id) AS rank') #Add comment count to post count and label the sum as "rank"
+        .joins(:posts) #Ties posts table to users table, via user_id
+        .joins(:comments) #Ties comments table to users table, via user_id
+        .group('users.id') #Instructs database to group results so each user will be returned in a distinct row
+        .order('rank DESC') #Instructs database to order results in descending order, by the rank that we created in this query. (rank = comment count + post count)
+   end
 end
