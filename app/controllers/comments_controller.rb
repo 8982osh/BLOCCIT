3afb392715aa1e97f8c96746718d148674217e0f
Cande Garcia <cruztoyota@gmail.com>
2016-02-26 00:12:30 -0700

    In progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,21 @@ def create
     else
       flash[:error] = "There was an error saving the comment. Please try again."
     end
+end
+
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    authorize @comment
+
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      #redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      #redirect_to [@post.topic, @post]
+    end
 
     respond_to do |format|
       format.html
@@ -22,6 +37,7 @@ def create
     end
   end
   
+
   private
 
   def comment_params
