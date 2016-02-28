39ba7f65ffd072141086c1b59d26d4b75eb1e900
Cande Garcia <cruztoyota@gmail.com>
2016-02-27 22:19:36 -0700

    Assignment 58 completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,32 @@
 class CommentsController < ApplicationController
 
-def create
+  def create
     @post = Post.find(params[:post_id])
     @comments = @post.comments
-
+  
     @comment = Comment.new( comment_params )
     @comment.user = current_user
     @comment.post = @post
     @new_comment = Comment.new
+    
     authorize @comment
 
     if @comment.save
       flash[:notice] = "Comment was created."
     else
       flash[:error] = "There was an error saving the comment. Please try again."
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    authorize @comment
+
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
     end
 
     respond_to do |format|
@@ -22,10 +35,10 @@ def create
     end
   end
   
+
   private
 
   def comment_params
-  params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body)
+    end
   end
-end
-
